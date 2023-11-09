36bd2d09e3b123ba498cff86a621b36ede6c1f74
BenBITC <138826466+BenBITC@users.noreply.github.com>
2023-11-09 14:48:14 -0600

    added google service links to header and implemented I'm feeling lucky button

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,27 @@
 import User from "./User";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Header() {
+    const router = useRouter()
+
     return (
         <header className="flex justify-between p-5 text-sm text-gray-700">
             <div className="flex space-x-4 items-center">
-                <p className="link">About</p>
-                <p className="link">Store</p>
+                <Link href="https://about.google.com">
+                    <p className="link">About</p>
+                </Link>
+                <Link href="https://store.google.com">
+                    <p className="link">Store</p>
+                </Link>
             </div>
             <div className="flex space-x-4 items-center">
-                <p className="link">Gmail</p>
-                <p className="link">Images</p>
+                <Link href="https://mail.google.com">
+                    <p className="link">Gmail</p>
+                </Link>
+                <Link href={`/search?term=${router.query.term || "Built Designs"}&searchType=image`}>
+                    <p className="link">Images</p>
+                </Link>
                 <User/>
             </div>
         </header>
